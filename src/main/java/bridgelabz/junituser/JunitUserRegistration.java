1943569b3b99955b7d5c991f0ed8c3d5ca7723d3
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-29 17:00:43 +0530

    Added JUnit for Password Rule3

--- src/main/java/bridgelabz/junituser/JunitUserRegistration.java
+++ src/main/java/bridgelabz/junituser/JunitUserRegistration.java
@@ -1,6 +1,5 @@
 package bridgelabz.junituser;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -48,5 +47,11 @@ public class JunitUserRegistration {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
+    public boolean passwordRule3(String password) {
+        regex = "^[a-z](?=.*[A-Z]+)(?=.*[0-9]+).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
-   
