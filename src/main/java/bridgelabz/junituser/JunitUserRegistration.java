4129d5bbfab26778f99aa330fcd8c16ae086bb80
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-29 16:16:11 +0530

    Added JUnit for Mobile Number

--- src/main/java/bridgelabz/junituser/JunitUserRegistration.java
+++ src/main/java/bridgelabz/junituser/JunitUserRegistration.java
@@ -26,6 +26,13 @@ public class JunitUserRegistration {
         Matcher matcher = pattern.matcher(emailIds);
         return matcher.matches();
     }
+
+    public boolean mobileNumber(String mobileNumber) {
+        regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mobileNumber);
+        return matcher.matches();
+    }
 }
 
-   
+  
