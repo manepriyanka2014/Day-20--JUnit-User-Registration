f020a89f0e9dacd002cb9822b849d902db5ce314
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-29 15:04:50 +0530

    Added JUnit User

---
+++ src/main/java/bridgelabz/junituser/App.java
@@ -0,0 +1,13 @@
+package bridgelabz.junituser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/bridgelabz/junituser/JunitUserRegistration.java
@@ -0,0 +1,14 @@
+package bridgelabz.junituser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JunitUserRegistration {
+    public String regex;
+
+    public boolean firstName(String firstName) {
+        regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+}
